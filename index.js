6e7ba0e6849f2a8763c9fa1ce5d396ef6339880e
dayojiboye <my@personal>
2022-03-12 16:50:05 +0100

    added confirm transaction modal

---
+++ index.js
@@ -0,0 +1,31 @@
+const registerForm = document.getElementById("register-form");
+const transferBtn = document.getElementById("transfer-btn");
+const confirmTransferModal = document.getElementById("confirm-transfer-modal");
+const confirmTransferModalContent = confirmTransferModal.querySelector(".tm-content");
+const confirmTransferModalCloseBtn = document.getElementById("tm-close-btn");
+const body = document.body;
+const html = document.documentElement;
+
+registerForm.addEventListener("submit", (e) => e.preventDefault());
+
+transferBtn.addEventListener("click", () => {
+	confirmTransferModal.classList.add("show");
+	html.classList.add("_fixed");
+	body.classList.add("_fixed");
+});
+
+confirmTransferModal.addEventListener("click", (e) => {
+	e.stopPropagation();
+	confirmTransferModal.classList.remove("show");
+	html.classList.remove("_fixed");
+	body.classList.remove("_fixed");
+});
+
+confirmTransferModalContent.addEventListener("click", (e) => e.stopPropagation());
+
+confirmTransferModalCloseBtn.addEventListener("click", (e) => {
+	e.stopPropagation;
+	confirmTransferModal.classList.remove("show");
+	html.classList.remove("_fixed");
+	body.classList.remove("_fixed");
+});
